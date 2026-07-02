9bc0f143c842abd81a31846f3e72c324b9820929
gnawlrak <gnawlrak@users.noreply.github.com>
2026-07-02 15:22:05 +0000

    feat: 查找项目歌词读取部分
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- smartisan-music/src/main/kotlin/io/github/proify/lyricon/smprovider/xposed/Constants.kt
+++ smartisan-music/src/main/kotlin/io/github/proify/lyricon/smprovider/xposed/Constants.kt
@@ -8,7 +8,7 @@ package io.github.proify.lyricon.smprovider.xposed
 
 object Constants {
     const val PROVIDER_PACKAGE_NAME: String = "io.github.proify.lyricon.smprovider"
-    const val MUSIC_PACKAGE_NAME: String = "com.smartisanos.music"
+    const val MUSIC_PACKAGE_NAME: String = "app.smartisanmusic.revived"
 
     const val ICON: String =
         "<svg t=\"1748538617911\" class=\"icon\" viewBox=\"0 0 1024 1024\" version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\" p-id=\"1484\" width=\"64\" height=\"64\"><path d=\"M864 256c0-53-43-96-96-96H256c-53 0-96 43-96 96v512c0 53 43 96 96 96h512c53 0 96-43 96-96V256zM736 352L480 608c-10 10-23 15-37 15h-50c-14 0-27-5-37-15l-80-80c-10-10-15-23-15-37s5-27 15-37c10-10 23-15 37-15s27 5 37 15l43 43 219-219c10-10 23-15 37-15s27 5 37 15c10 10 15 23 15 37s-5 27-15 37z\" p-id=\"1485\"></path></svg>"
